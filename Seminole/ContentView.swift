bb8b076236d2ade8a5190a113d21238484fcf68d
Pavel Ivanov <p.ivanov@citydrive.ru>
2025-03-13 18:58:34 +0300

    Initial Commit

---
+++ Seminole/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Seminole
+//
+//  Created by Pavel Ivanov on 13.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Seminole/SeminoleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeminoleApp.swift
+//  Seminole
+//
+//  Created by Pavel Ivanov on 13.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct SeminoleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
